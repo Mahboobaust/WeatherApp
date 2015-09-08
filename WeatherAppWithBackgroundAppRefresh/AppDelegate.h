9d173f9f12426101170ecc788db32a2df8796476
Mahboobaust <mahboobaust13@gmail.com>
2015-09-08 13:59:00 +0600

    Initial Commit

---
+++ WeatherAppWithBackgroundAppRefresh/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeatherAppWithBackgroundAppRefresh
+//
+//  Created by Mahboob on 9/8/15.
+//  Copyright (c) 2015 Mahboob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeatherAppWithBackgroundAppRefreshTests/WeatherAppWithBackgroundAppRefreshTests.m
@@ -0,0 +1,34 @@
+//
+//  WeatherAppWithBackgroundAppRefreshTests.m
+//  WeatherAppWithBackgroundAppRefreshTests
+//
+//  Created by Mahboob on 9/8/15.
+//  Copyright (c) 2015 Mahboob. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WeatherAppWithBackgroundAppRefreshTests : XCTestCase
+
+@end
+
+@implementation WeatherAppWithBackgroundAppRefreshTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
